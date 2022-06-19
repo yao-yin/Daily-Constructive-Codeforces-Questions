65bed26a03b1602e7bd53f907ce67f01cad2065d
Yao-Yin <Yao-Yin1997@users.noreply.github.com>
2022-06-20 07:27:57 +0900

    Carousel (1800)

---
+++ 1800/Carousel.cpp
@@ -0,0 +1,101 @@
+// 20220620
+// https://codeforces.com/problemset/problem/1328/D
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <climits>
+#include <algorithm>
+#include <numeric>
+#include <utility>
+#include <random>
+#include <chrono>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+//const int mod = 1e9+7;
+int q, n;
+
+inline void quickread() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+void fixup(vector<int> & t, vector<int> & color) {
+    bool flag = false;
+    for (int i = 1; i < n; i ++) {
+        if (t[i] == t[i-1]) {
+            if (!flag) {
+                color[i] = 3 - color[i-1];
+                flag = true;
+            } else {
+                color[i] = color[i-1];
+            }
+        } else {
+            color[i] = 3 - color[i-1];
+        }
+    }
+    if (color[n-1] == color[0] && t[n-1] != t[0]) {
+        color[0] = 3;
+        for (int i = 1; i < n; i ++) {
+            if (t[i] == t[i-1]){
+                color[i] = color[i-1];
+            } else {
+                break;
+            }
+        }
+    } 
+    cout << *max_element(color.begin(), color.end()) << endl;
+    for (int i = 0; i < n; i ++) {
+        if (i) cout << " ";
+        cout << color[i];
+    }
+    cout << endl;
+}
+
+void solve() {
+    cin >> n;
+    vector<int> t(n, 0);
+    for (int i = 0; i < n; i ++) {
+        cin >> t[i];
+    }
+    vector<int> color(n, 0);
+    color[0] = 1;
+    for (int i = 1; i < n; i ++) {
+        if (t[i] == t[i-1]) {
+            color[i] = color[i-1];
+        } else {
+            color[i] = 3 - color[i-1];
+        }
+    }
+
+    if(color[n-1] == color[0] && t[n-1] != t[0]) {
+        fixup(t, color);
+        return;
+    }
+    cout << *max_element(color.begin(), color.end()) << endl;
+    for (int i = 0; i < n; i ++) {
+        if (i) cout << " ";
+        cout << color[i];
+    }
+    cout << endl;
+}
+
+int main() {
+    quickread();
+    cin >> q;
+    while (q --) {
+        solve();
+    }    
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Daily-Constructive-Codeforces-Questions
 A constructive question a day, keep the stupidity away.
+
+## Why
+
+I have ~2400 leetcode contest rating, most of leetcode problems are not challenging and interesting. Meanwhile my codeforces rating and atcoder rating is stuck at around 1600-1700 and 1400-1500, respectively. Get inspired from [the Zhihu answer](https://www.zhihu.com/question/339795230/answer/868748153) and [the codeforces comment](https://codeforces.com/blog/entry/66715?#comment-507869), I decide to follow this way to improve my problem solving ability.
+
+## A deeper why
+
+TODO
+
+## Plan
+
+My current rating is 1658 (max. 1748) on codeforces. I will pick questions with constructive algorithms label in range {1800, 1900, 2000} (roughly my rating + 200), and solve them 1 per weekday on average (I might be super busy on Wed). 
+
+Season #1:
+June 20th, 2022 ~ September 18th, 2022
